efbd36440987b6a7b84e4d0168e075158a2a566e
John Hardy <jh@lagado.com>
2019-09-18 14:14:33 +1000

    experiments

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
+<svelte:head>
+    <link rel="shortcut icon" href="../assets/favicon.ico">
+  	<title>Svelte app!</title>
+</svelte:head>
+
 <script>
-	import Nested from './Nested.svelte';
+  import Nested from './Nested.svelte';
+  import { greet } from './util.js';
+
+  greet();
 </script>
 
 <Nested answer={42}/>
